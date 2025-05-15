6ff975dba7d178c3e97e536f598ea05aaed197e6
grantimaharafoundation <michael@grantimaharafoundation.org>
2025-05-15 14:43:05 -0700

    Take 2

--- lib/pbio/sys/main.c
+++ lib/pbio/sys/main.c
@@ -84,12 +84,16 @@ int main(int argc, char **argv) {
 
     pbio_init();
     pbsys_init();
+    static bool initial_program_auto_started = false;
 
     // Keep loading and running user programs until shutdown is requested.
     while (!pbsys_status_test(PBIO_PYBRICKS_STATUS_SHUTDOWN_REQUEST)) {
 
-        // Always attempt to start the program in the first slot on boot.
-        //pbsys_main_program_request_start(PBIO_PYBRICKS_USER_PROGRAM_ID_FIRST_SLOT, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_BOOT);
+        if (!initial_program_auto_started) {
+            // Attempt to start the program in the first slot on initial boot.
+            pbsys_main_program_request_start(PBIO_PYBRICKS_USER_PROGRAM_ID_FIRST_SLOT, PBSYS_MAIN_PROGRAM_START_REQUEST_TYPE_BOOT);
+            initial_program_auto_started = true;
+        }
 
         // REVISIT: this can be long waiting, so we could do a more efficient
         // wait (i.e. __WFI() on embedded system)
